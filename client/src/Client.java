15a7f49b06325b93df3b9522e6c9aefdfd466f06
Pedro <125268185+jkaxsh@users.noreply.github.com>
2024-05-19 15:01:36 +0100

    accs testing

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,20 +1,35 @@
+
 public class Client {
+    private static final int NULL = 0;
+    private static final int CREATE_ACCOUNT = 1;
+    private static final int LOGIN_ACCOUNT = 2;
+    private static final int LOGOUT_ACCOUNT = 3;
+    private static final int JOIN_ROOM = 4;
+    private static final int LEAVE_ROOM = 5;
+
     public static void main(String[] args) {
         try {
-            if ( args.length < 2) {
-                print("Example usage: java client [host] [port]");
-                System.exit(1);
-            }
 
-            TCP tcp = new TCP(args[0], Integer.parseInt(args[1]));
-
-
+            TCP tcp = new TCP("localhost", 12345);
+            System.out.println(tcp.receive());
+            System.out.println(tcp.pingpong(LOGOUT_ACCOUNT));
+            System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "buendia@@@buedafixe"));
+            System.out.println(tcp.pingpong(LEAVE_ROOM));
+            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
+            System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendia@@@buedafixe"));
+            System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendia@@@akfgnj"));
+            System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "buendia@@@buedafixe"));
+            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
+            System.out.println(tcp.pingpong(LEAVE_ROOM));
+            System.out.println(tcp.pingpong(LOGOUT_ACCOUNT));
+            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
         } catch (Exception e) {
             print(e.getMessage());
         }
     }
 
     public static void print(String message) {
+
         System.out.println(message);
     }
 }
--- client/src/TCP.java
+++ client/src/TCP.java
@@ -1,28 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.List;
+import java.util.Objects;
 
 public class TCP {
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
-    StringBuilder sb = new StringBuilder();
 
     public TCP(String host, int port) throws IOException {
         this.socket = new Socket(host, port);
-        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.out = new PrintWriter(socket.getOutputStream());
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.out = new PrintWriter(this.socket.getOutputStream());
     }
 
-    public void send(String message) {
-        out.println(message);
+    public String pingpong(int type, String args) throws IOException {
+        this.send(type, args);
+        return this.receive();
+    }
+
+    public String pingpong(int type) throws IOException {
+        this.send(type);
+        return this.receive();
+    }
+
+    void send(int type) throws IOException {
+        out.println(type + "@@@");
+        out.flush();
+    }
+
+    void send(int type, String args) throws IOException {
+        String result = type + "@@@" + args;
+        out.println(result);
         out.flush();
     }
 
     public String receive() throws IOException {
-        String message = in.readLine();
-        return message;
+        StringBuilder line = new StringBuilder();
+        String current = in.readLine();
+        while (!Objects.equals(current, "!-SVDONE-!")){
+            line.append(current);
+            current = in.readLine();
+        }
+        return line.toString();
     }
 }
