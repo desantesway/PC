ddb2f6155c1567244019aad2dc1d7d9191884532
Pedro <125268185+jkaxsh@users.noreply.github.com>
2024-05-19 19:13:21 +0100

    changing accs tests

--- client/src/Client.java
+++ client/src/Client.java
@@ -6,23 +6,32 @@ public class Client {
     private static final int LOGOUT_ACCOUNT = 3;
     private static final int JOIN_ROOM = 4;
     private static final int LEAVE_ROOM = 5;
+    private static final int CHANGE_NAME = 6;
+    private static final int CHANGE_PASS = 7;
+    private static final int REMOVE_ACCOUNT = 8;
 
     public static void main(String[] args) {
         try {
 
             TCP tcp = new TCP("localhost", 12345);
             System.out.println(tcp.receive());
-            System.out.println(tcp.pingpong(LOGOUT_ACCOUNT));
+            /*System.out.println(tcp.pingpong(LOGOUT_ACCOUNT));
             System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "buendia@@@buedafixe"));
             System.out.println(tcp.pingpong(LEAVE_ROOM));
             System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
-            System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendia@@@buedafixe"));
+            System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendiffdfa@@@buedafixe"));
             System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendia@@@akfgnj"));
             System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "buendia@@@buedafixe"));
             System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
             System.out.println(tcp.pingpong(LEAVE_ROOM));
             System.out.println(tcp.pingpong(LOGOUT_ACCOUNT));
-            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
+            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));*/
+            System.out.println(tcp.pingpong(CHANGE_NAME, "arroz"));
+            System.out.println(tcp.pingpong(CHANGE_PASS, "verycool"));
+            System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "arrozado@@@cool"));
+            System.out.println(tcp.pingpong(CHANGE_NAME, "ei"));
+            System.out.println(tcp.pingpong(CHANGE_PASS, "yo"));
+
         } catch (Exception e) {
             print(e.getMessage());
         }
