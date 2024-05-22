93f056c9aa3355d5d8a008ec16bd0382f7865f36
Pedro <125268185+jkaxsh@users.noreply.github.com>
2024-05-22 14:36:15 +0100

    Client for game win/lose and gain xp

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,4 +1,3 @@
-
 public class Client {
     private static final int NULL = 0;
     private static final int CREATE_ACCOUNT = 1;
@@ -11,10 +10,31 @@ public class Client {
     private static final int REMOVE_ACCOUNT = 8;
     private static final int CREATE_ROOM = 9;
     private static final int LIST_ROOMS = 10;
+    private static final int UP_KEY = 11;
+    private static final int RIGHT_KEY = 12;
+    private static final int LEFT_KEY = 13;
+    private static final int CHAT_MESSAGE = 14;
 
     public static void main(String[] args) {
         try {
 
+            TCP tcp = new TCP("localhost", 12345);
+            System.out.println(tcp.pingpong(CREATE_ACCOUNT, "buendia@@@akfgnj"));
+            System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "buendia@@@akfgnj"));
+            System.out.println(tcp.pingpong(CREATE_ROOM, "1"));
+            System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
+            System.out.println(tcp.receive());
+            System.out.println(tcp.receive());
+            System.out.println(tcp.receive());
+            System.out.println(tcp.receive());
+            Thread.sleep(10000); // 10sec para ganhar, 2sec para perder
+            tcp.send(UP_KEY);
+            while(true){
+                //tcp.send(CHAT_MESSAGE, "eu e os meus tropas, somos bandidos");
+                System.out.println(tcp.receive());
+            }
+
+            /* for another player
             TCP tcp = new TCP("localhost", 12345);
             System.out.println(tcp.pingpong(CREATE_ACCOUNT, "arroz@@@buedafixe"));
             System.out.println(tcp.pingpong(LOGIN_ACCOUNT, "arroz@@@buedafixe"));
@@ -23,10 +43,14 @@ public class Client {
             System.out.println(tcp.pingpong(CREATE_ROOM, "3"));
             System.out.println(tcp.pingpong(LIST_ROOMS));
             System.out.println(tcp.pingpong(JOIN_ROOM, "1"));
+            System.out.println(tcp.receive());
+            System.out.println(tcp.receive());
+            tcp.send(UP_KEY); / perder
             while(true){
                 System.out.println(tcp.receive());
             }
-            //Thread.sleep(10000);
+            */
+
         } catch (Exception e) {
             print(e.getMessage());
         }
