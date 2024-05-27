e786e35db7eb43edfdb10c94d19b0407ba004cfd
Diogo Alexandre Ribeiro da Costa <85320791+Costinha96@users.noreply.github.com>
2024-05-27 02:28:35 +0100

    added client-side multi-threaded part

--- client/src/TCP.java
+++ client/src/TCP.java
@@ -1,106 +1,68 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.List;
-import java.util.Objects;
-public class TCP {
-    // TODO - Fix this before upload
-    private static final int NULL = 0;
-    private static final int CREATE_ACCOUNT = 1;
-    private static final int LOGIN_ACCOUNT = 2;
-    private static final int LOGOUT_ACCOUNT = 3;
-    private static final int JOIN_ROOM = 4;
-    private static final int LEAVE_ROOM = 5;
-    private static final int CHANGE_NAME = 6;
-    private static final int CHANGE_PASS = 7;
-    private static final int REMOVE_ACCOUNT = 8;
-    private static final int CREATE_ROOM = 9;
-    private static final int LIST_ROOMS = 10;
-    private static final int UP_KEY = 11;
-    private static final int RIGHT_KEY = 12;
-    private static final int LEFT_KEY = 13;
-    private static final int CHAT_MESSAGE = 14;
-    private static final int LEAVE_CHAT = 15;
-    private static final int RANKING = 16;
+import java.util.*;
 
+public class TCP {
     //Lock l = new ReentrantLock();
-    private Socket socket;
+    private final Socket socket;
     private BufferedReader in;
     private PrintWriter out;
+    private Thread postman;
+    private Map<String,Queue<String>> taskMap;
 
-    public TCP(String host, int port) throws IOException {
+
+    private void taskMapper() {
+        this.taskMap = new HashMap<>();
+        this.taskMap.put("pos1", new LinkedList<>());
+        this.taskMap.put("pos2", new LinkedList<>());
+        this.taskMap.put("pos3", new LinkedList<>());
+        this.taskMap.put("pos4", new LinkedList<>());
+        this.taskMap.put("p1", new LinkedList<>());
+        this.taskMap.put("p2", new LinkedList<>());
+        this.taskMap.put("p3", new LinkedList<>());
+        this.taskMap.put("p4", new LinkedList<>());
+        this.taskMap.put("res", new LinkedList<>());
+        this.taskMap.put("game", new LinkedList<>());
+        this.taskMap.put("chat", new LinkedList<>());
+        // Add meteor types
+    }
+
+    public TCP(String host, int port) throws IOException, IllegalThreadStateException {
         this.socket = new Socket(host, port);
         this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         this.out = new PrintWriter(this.socket.getOutputStream());
-    } 
-
-    public String pingpong(int type, String args) throws IOException {
-        this.send(type, args);
-        return this.receive();
-    }
-    
-    public String register_user(String username, String password) throws IOException {
-        this.send(CREATE_ACCOUNT, username + "@@@" + password);
-        return this.receive();
-    }
-
-    public String login_user(String username, String password) throws IOException {
-        this.send(LOGIN_ACCOUNT, username + "@@@" + password);
-        return this.receive();
-    }
-    
-    public String create_room(String room) throws IOException {
-      this.send(CREATE_ROOM, room);
-      return this.receive();
-    }
-    
-    public String join_room(String room) throws IOException {
-      this.send(JOIN_ROOM, room);
-      return this.receive();
-    }
-    
-    public String leave_room(String room) throws IOException {
-      this.send(LEAVE_ROOM, room);
-      return this.receive();
-    }
-  
-    public String list_rooms() throws IOException { // Needed a temporary hack. Server is sending Message ++ "\n". And in.readLine() filters '\n'
-      this.send(LIST_ROOMS, "Lol");
-      StringBuilder line = new StringBuilder();
-      String current = in.readLine();
-      while (!Objects.equals(current, "!-SVDONE-!")){
-        if (current != null){
-            line.append(current);
-        }
-        current = in.readLine();
-      }
-      System.out.println(line.toString());
-      return line.toString();
-    }
-    
-    
-    public String pingpong(int type) throws IOException {
-        this.send(type);
-        return this.receive();
-    }
-    
-    private void send(int type) throws IOException {
-        out.println(type + "@@@");
-        out.flush();
+        taskMapper();
+        this.postman = new Thread(() -> {
+            String res;
+            try {
+                while((res = in.readLine()) != null) {
+                    String[] task = res.split("@@@",2); // Split into <Task type> , <Task>
+                    Queue<String> tasks = this.taskMap.get(task[0]); // get the Queue from the HashMap
+                    synchronized (tasks) { // Add the task to the Queue and notify main thread
+                        tasks.add(task[1]); 
+                        tasks.notify();
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Failed to get message - postman.");
+            }
+        });
+        this.postman.start();
     }
 
-    private void send(int type, String args) throws IOException {
+    public void send(int type, String args) throws IOException {
         String result = type + "@@@" + args;
         out.println(result);
         out.flush();
     }
 
-    public String receive() throws IOException {
-        StringBuilder line = new StringBuilder();
-        String current = in.readLine();
-        while (!Objects.equals(current, "!-SVDONE-!")){
-            line.append(current);
-            current = in.readLine();
+    public String receive(String task) throws IOException, InterruptedException {
+        Queue<String> tasks = this.taskMap.get(task);
+        synchronized (tasks) {
+            while (tasks.isEmpty()) {
+                tasks.wait();
+            }
+            return tasks.remove();
         }
-        return line.toString();
     }
 }
